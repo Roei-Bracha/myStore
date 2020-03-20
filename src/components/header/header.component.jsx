3c7f4acf4f40340e3ef067861ba209dd9203d7c3
roei.bracha@otorio.io <roei635@gmail.com>
2020-03-20 19:25:50 +0200

    add some styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {createStructuredSelector} from "reselect";
 
 import {auth} from '../../firebase/firebase.utils'
 
-import './header.styles.scss';
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import {selectCartHidden} from "../../redux/cart/cart.selectors";
 import {selectCurrentUser} from "../../redux/user/user.selectors";
+import {HeaderComponent, LogoContainer, OptionDiv, OptionLink, OptionsContainer} from "./header.styles";
 
 const Header = ({ currentUser, hidden}) => {
   return (
-    <div className='header'>
-      <Link className='logo-container' to='/'>
+    <HeaderComponent>
+      <LogoContainer to='/'>
         <Logo className='logo' />
-      </Link>
-      <div className='options'>
-        <Link className='option' to='/shop'>
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink  to='/shop'>
           SHOP
-      </Link>
-        <Link className='option' to='/shop'>
+      </OptionLink>
+        <OptionLink  to='/shop'>
           CONTACT
-      </Link>
+      </OptionLink>
         {currentUser ?
-          <div className='option' onClick={() => auth.signOut()}>
+          <OptionDiv onClick={() => auth.signOut()}>
             SIGN OUT
-        </div>
+        </OptionDiv>
           :
-          <Link className='option' to='/signin'>
+          <OptionLink to='/signin'>
             Sign In
-        </Link>
+          </OptionLink>
         }
         <CartIcon/>
-      </div>
+      </OptionsContainer>
       {hidden ? null : <CartDropdown />}
-    </div>
+    </HeaderComponent>
   )
 };
 
